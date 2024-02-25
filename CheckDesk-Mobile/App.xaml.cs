6170484de0fecbe9a969809dd86c1f6b98964aa9
flmts <florian.montastier@outlook.fr>
2024-02-25 15:58:13 +0100

    Ajoutez des fichiers projet.

---
+++ CheckDesk-Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CheckDesk_Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CheckDesk-Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckDesk_Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
